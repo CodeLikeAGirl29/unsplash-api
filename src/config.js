f2f81355bd97f9713f38669f9f305391f247fe10
codelikeagirl29 <miss_leading009@yahoo.com>
2023-04-10 21:51:56 -0400

    remove api key

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import { createApi } from 'unsplash-js';
 
-const APP_ACCESS_KEY = 'Tkbei_onnYA12fX5wPjNP2UCb0Lx55pRR0doZZfB1ls';
+const APP_ACCESS_KEY = '';
 
 const unsplash = createApi({
     accessKey: APP_ACCESS_KEY
