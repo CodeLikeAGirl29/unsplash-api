9db61ae9de3134d2f6525527c09a1fe75bb18423
Lindsey Howard <codelikeagirl91@gmail.com>
2023-04-30 13:48:38 -0400

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "settings": {
+        "node": {
+            "allowModules": ["electron"],
+            "resolvePaths": [__dirname],
+            "tryExtensions": [".js", ".json", ".node"]
+        }
+    },
+    "rules": {
+        "node/no-missing-import": "error"
+    }
+}
